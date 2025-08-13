c27f6169a03e5eeb16d24a4ff94056cc58ae81f1
bilaldev2 <bilal.developer.arcly@gmail.com>
2025-08-14 00:06:48 +0530

    cors config

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const source = process.env.MONGODB_URI; 
+const source = process.env.MONGODB_URI;
 
-
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const eventsRoutes = require('./routes/events');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const eventsRoutes = require("./routes/events");
 
 const app = express();
 
@@ -15,27 +14,32 @@ const PORT = parseInt(process.env.PORT, 10) || 5000;
 // console.log(source);
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://gloria-hyav.onrender.com/"], // exact frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
-app.use('/api/events', eventsRoutes);
+app.use("/api/events", eventsRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.post('/belal/', (req, res) => {
-  res.send('HOLA AMIGO')
+app.post("/belal/", (req, res) => {
+  res.send("HOLA AMIGO");
   console.log(`went to belal page`);
-  
-})
-mongoose.connect(source)
+});
+mongoose
+  .connect(source)
   .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () =>
+      console.log(`Server running on port http://localhost:${PORT}`)
+    );
   })
-  .catch((error) => console.error('Error connecting to MongoDB:', error));
-  
-
-  
+  .catch((error) => console.error("Error connecting to MongoDB:", error));
